b824f105fa3491759f41c3156cb08e21baa12fda
starfishwrx <a358101523@gmail.com>
2026-07-21 16:25:34 +0800

    chore: preserve staged slide workspace

--- slides/publish-p3.sh
+++ slides/publish-p3.sh
@@ -5,7 +5,7 @@ SRC_DIR="/Users/starfish/Documents/test1/detademo/autodatareport/slides"
 DST_DIR="/Users/starfish/.local/share/p3-site"
 
 mkdir -p "$DST_DIR"
-cp "$SRC_DIR/ai-workflow-defense-v8.html" "$DST_DIR/index.html"
+cp "$SRC_DIR/ai-workflow-defense-v9.html" "$DST_DIR/index.html"
 cp "$SRC_DIR/gui-screenshot-2026-03-17-001159.png" "$DST_DIR/"
 cp "$SRC_DIR/final-delivery-result.png" "$DST_DIR/"
 
